982d109660144189050c31169c3f52572a7b2f1c
Priyanshu Chaturvedi <chaturvedi.priyanshu11@gmail.com>
2022-11-29 09:48:33 +0530

    comm

--- assets/js/main.js
+++ assets/js/main.js
@@ -206,3 +206,25 @@ const text = document.querySelector(".sec-text");
 
         textLoad();
         setInterval(textLoad, 12000);
+
+
+
+
+
+
+
+       
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6385862bb0d6371309d1a03a/1gj0p86vl';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
+
+window.addEventListener('load',function(){
+  document.querySelector('body').classList.add("loaded")  
+});
